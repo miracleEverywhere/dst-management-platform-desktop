6fa5c491d4b388c80571a15f49bef0d1c2abf6ca
Miracle <763483966@qq.com>
2025-03-07 10:21:52 +0800

    init

--- src/layouts/components/NavItems.vue
+++ src/layouts/components/NavItems.vue
@@ -11,41 +11,41 @@
   <VerticalNavGroup
       :item="{
       title: '设置',
-      icon: 'ri-home-smile-line',
+      icon: 'ri-settings-line',
     }"
   >
     <VerticalNavLink
         :item="{
         title: '房间',
-        icon: 'ri-home-smile-line',
+        icon: 'ri-gamepad-fill',
         to: '/settings/room',
       }"
     />
     <VerticalNavLink
         :item="{
         title: '玩家',
-        icon: 'ri-home-smile-line',
+        icon: 'ri-user-heart-fill',
         to: '/settings/player',
       }"
     />
     <VerticalNavLink
         :item="{
         title: '模组',
-        icon: 'ri-home-smile-line',
+        icon: 'ri-file-settings-fill',
         to: '/settings/mod',
       }"
     />
     <VerticalNavLink
         :item="{
         title: '导入',
-        icon: 'ri-home-smile-line',
+        icon: 'ri-import-fill',
         to: '/settings/import',
       }"
     />
     <VerticalNavLink
         :item="{
         title: '系统',
-        icon: 'ri-home-smile-line',
+        icon: 'ri-settings-4-fill',
         to: '/settings/system',
       }"
     />
@@ -54,48 +54,48 @@
   <VerticalNavGroup
       :item="{
       title: '工具',
-      icon: 'ri-home-smile-line',
+      icon: 'ri-tools-line',
     }"
   >
     <VerticalNavLink
         :item="{
         title: '定时更新',
-        icon: 'ri-home-smile-line',
+        icon: 'ri-download-cloud-fill',
         to: '/tools/update',
       }"
     />
     <VerticalNavLink
         :item="{
         title: '定时备份',
-        icon: 'ri-home-smile-line',
+        icon: 'ri-save-2-fill',
         to: '/tools/backup',
       }"
     />
     <VerticalNavLink
         :item="{
         title: '定时通知',
-        icon: 'ri-home-smile-line',
+        icon: 'ri-notification-3-fill',
         to: '/tools/announce',
       }"
     />
     <VerticalNavLink
         :item="{
         title: '安装游戏',
-        icon: 'ri-home-smile-line',
+        icon: 'ri-install-fill',
         to: '/tools/install',
       }"
     />
     <VerticalNavLink
         :item="{
         title: '玩家统计',
-        icon: 'ri-home-smile-line',
+        icon: 'ri-bar-chart-box-ai-fill',
         to: '/tools/statistics',
       }"
     />
     <VerticalNavLink
         :item="{
         title: '系统监控',
-        icon: 'ri-home-smile-line',
+        icon: 'ri-line-chart-fill',
         to: '/tools/metrics',
       }"
     />
@@ -104,48 +104,48 @@
   <VerticalNavGroup
       :item="{
       title: '日志',
-      icon: 'ri-home-smile-line',
+      icon: 'ri-blogger-line',
     }"
   >
     <VerticalNavLink
         :item="{
         title: '地面日志',
-        icon: 'ri-home-smile-line',
+        icon: 'ri-sun-fill',
         to: '/logs/ground',
       }"
     />
     <VerticalNavLink
         :item="{
         title: '洞穴日志',
-        icon: 'ri-home-smile-line',
+        icon: 'ri-typhoon-fill',
         to: '/logs/cave',
       }"
     />
     <VerticalNavLink
         :item="{
         title: '聊天日志',
-        icon: 'ri-home-smile-line',
+        icon: 'ri-chat-smile-fill',
         to: '/logs/chat',
       }"
     />
     <VerticalNavLink
         :item="{
         title: '请求日志',
-        icon: 'ri-home-smile-line',
+        icon: 'ri-code-box-fill',
         to: '/logs/dmp',
       }"
     />
     <VerticalNavLink
         :item="{
         title: '运行日志',
-        icon: 'ri-home-smile-line',
+        icon: 'ri-cpu-line',
         to: '/logs/runtime',
       }"
     />
     <VerticalNavLink
         :item="{
         title: '清理日志',
-        icon: 'ri-home-smile-line',
+        icon: 'ri-file-shred-fill',
         to: '/logs/clean',
       }"
     />
@@ -154,7 +154,7 @@
   <VerticalNavLink
       :item="{
         title: '帮助',
-        icon: 'ri-home-smile-line',
+        icon: 'ri-heart-2-line',
         to: '/help',
       }"
   />
