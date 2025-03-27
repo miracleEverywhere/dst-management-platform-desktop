4b3dfa71dc9dfbf46ea8c046eb6780b78f338569
Miracle <763483966@qq.com>
2025-03-27 21:59:10 +0800

    格式优化

--- src/utils/electronApi.js
+++ src/utils/electronApi.js
@@ -1,33 +1,33 @@
 export default class ElectronApi {
-    electronAPI;
-    static store = {
-        get(key) {
-            return window.electronAPI.getStoreValue(key)
-        },
-        set(key, value) {
-            window.electronAPI.setStoreValue(key, value)
-        },
-        delete(key) {
-            window.electronAPI.deleteStoreValue(key)
-        },
-        append(key, value) {
-            let dbValue = window.electronAPI.getStoreValue(key)
-            dbValue.push(value)
-            this.set(key, dbValue)
-        },
-        clear() {
-            window.electronAPI.clearStoreValue()
-        }
+  static store = {
+    get(key) {
+      return window.electronAPI.getStoreValue(key)
+    },
+    set(key, value) {
+      window.electronAPI.setStoreValue(key, value)
+    },
+    delete(key) {
+      window.electronAPI.deleteStoreValue(key)
+    },
+    append(key, value) {
+      let dbValue = window.electronAPI.getStoreValue(key)
+      dbValue.push(value)
+      this.set(key, dbValue)
+    },
+    clear() {
+      window.electronAPI.clearStoreValue()
     }
-    static window = {
-        dashboard() {
-            window.electronAPI.openDashboardWindow()
-        },
-        config() {
-            window.electronAPI.openConfigWindow()
-        },
-        reload() {
-            window.electronAPI.reloadWindow()
-        }
-    }
+  }
+  static window = {
+    dashboard() {
+      window.electronAPI.openDashboardWindow()
+    },
+    config() {
+      window.electronAPI.openConfigWindow()
+    },
+    reload() {
+      window.electronAPI.reloadWindow()
+    },
+  }
+  electronAPI;
 }
