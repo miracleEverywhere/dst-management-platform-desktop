97b1fa63837723457bb630a5fcf53bf35d89cd00
Miracle <763483966@qq.com>
2025-04-01 19:01:02 +0800

    添加loading

--- src/views/settings/system.vue
+++ src/views/settings/system.vue
@@ -11,8 +11,8 @@
       </div>
     </v-card-title>
     <v-card-text>
-      <v-container height="700" style="overflow-y: auto">
-        <v-form ref="roomBaseFormRef" class="mt-4" fast-fail>
+      <v-container v-if="!loading" height="700" style="overflow-y: auto">
+        <v-form ref="systemSettingFormRef" class="mt-4" fast-fail>
           <v-alert density="compact" color="primary" class="mb-4">自动保活</v-alert>
           <v-row>
             <v-col cols="3">
@@ -188,6 +188,7 @@
 
         </v-form>
       </v-container>
+      <sc-loading v-else :height="700"></sc-loading>
     </v-card-text>
   </v-card>
 </template>
@@ -200,10 +201,14 @@ import toolsApi from "@/api/tools";
 import {showSnackbar} from "@/utils/snackbar";
 import {VNumberInput} from "vuetify/labs/VNumberInput";
 
-onMounted(() => {
-  handleGetSystemSetting()
-  handleGetOSPlatform()
+onMounted(async () => {
+  loading.value = true
+  await handleGetSystemSetting()
+  await handleGetOSPlatform()
 })
+
+// 数据获取不成功就一直加载，不允许修改页面
+const loading = ref(false)
 
 const systemSettingFormRef = ref()
 const systemSettingFormOld = ref({
@@ -270,32 +275,32 @@ const handleGetSystemSetting = () => {
 }
 
 const submitButtonLoading = ref(false)
-const handleSubmit = () => {
-  systemSettingFormRef.value.validate(valid => {
-    if (valid) {
-      if (_.isEqual(systemSettingFormOld.value, systemSettingForm.value)) {
-        showSnackbar('配置未修改', 'error')
-      } else {
-        submitButtonLoading.value = true
-        settingApi.system.setting.put(systemSettingForm.value).then(response => {
-          handleGetSystemSetting()
-          showSnackbar(response.message)
-          if (systemSettingFormOld.value.bit64 !== systemSettingForm.value.bit64) {
-            const message = '正在后台切换32/64，可依据CPU使用率判断是否执行完毕，执行完毕后重启游戏即可。'
-            showSnackbar(message, 'info')
-          }
-        }).finally(() => {
-          submitButtonLoading.value = false
-        })
-      }
+const handleSubmit = async () => {
+  const {valid} = await systemSettingFormRef.value.validate();
+  if (valid) {
+    if (_.isEqual(systemSettingFormOld.value, systemSettingForm.value)) {
+      showSnackbar('配置未修改', 'error')
+    } else {
+      submitButtonLoading.value = true
+      settingApi.system.setting.put(systemSettingForm.value).then(response => {
+        handleGetSystemSetting()
+        showSnackbar(response.message)
+        if (systemSettingFormOld.value.bit64 !== systemSettingForm.value.bit64) {
+          const message = '正在后台切换32/64，可依据CPU使用率判断是否执行完毕，执行完毕后重启游戏即可。'
+          showSnackbar(message, 'info')
+        }
+      }).finally(() => {
+        submitButtonLoading.value = false
+      })
     }
-  })
+  }
 }
 
 const OSPlatform = ref("")
 const handleGetOSPlatform = () => {
   toolsApi.osInfo.get().then(response => {
     OSPlatform.value = response.data.Platform
+    loading.value = false
   })
 }
 </script>
