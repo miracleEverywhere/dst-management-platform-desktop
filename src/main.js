ffbc6ed8f737f9937484fc80bc388ebfe329e57a
miracleEverywhere <763483966@qq.com>
2025-08-20 16:39:20 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,20 +2,25 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 import { registerPlugins } from '@core/utils/plugins'
 import directives from "@/directives";
-
+import ganttastic from "@infectoone/vue-ganttastic";
+import dayjs from "dayjs";
+import 'dayjs/locale/zh';
+import 'dayjs/locale/en';
 // Styles
 import '@core/scss/template/index.scss'
 import '@layouts/styles/index.scss'
 import '@/styles/custom.css'
 
+dayjs.locale('zh')
+
 // Create vue app
 const app = createApp(App)
-
 
 // Register plugins
 registerPlugins(app)
 
 app.use(directives)
+app.use(ganttastic)
 
 // Mount vue app
 app.mount('#app')
