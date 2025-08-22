93c282c731f84d469124aa9477d1d66bf8663807
miracleEverywhere <763483966@qq.com>
2025-08-22 15:35:33 +0800

    update

--- src/views/settings/system.vue
+++ src/views/settings/system.vue
@@ -26,7 +26,7 @@
                               v-tooltip:top="'单位秒，默认为30秒，间隔越小，平台更新游戏玩家信息越及时，但会带来额外的性能消耗和更乱的世界日志'"
                               :min="1"
                               variant="outlined"
-                              control-variant="stacked" inset label="玩家列表获取频率">
+                              control-variant="hidden" inset label="玩家列表获取频率">
                 <template #append-inner>
                   <div style="width: 50px">秒</div>
                 </template>
@@ -42,7 +42,7 @@
             <v-col>
               <v-radio-group v-model="systemSettingForm.schedulerSetting.UIDMaintain.disable"
                              v-tooltip:top="'默认开启，关闭后，在设置-玩家的管理员黑名单白名单页面中将不再显示玩家昵称，历史玩家信息也不会再新增'"
-                             inline class="mt-2">
+                             inline color="error" class="mt-2">
                 <template #prepend>
                   <span>
                     玩家昵称字典维护开关
@@ -60,7 +60,7 @@
                               v-tooltip:top="'单位分钟，默认为5分钟，即更新昵称字典的间隔'"
                               :min="1"
                               :disabled="systemSettingForm.schedulerSetting.UIDMaintain.disable"
-                              control-variant="stacked" inset label="玩家昵称字典写入频率" variant="outlined">
+                              control-variant="hidden" inset label="玩家昵称字典写入频率" variant="outlined">
                 <template #append-inner>
                   <div style="width: 50px">分钟</div>
                 </template>
@@ -76,7 +76,7 @@
             <v-col>
               <v-radio-group v-model="systemSettingForm.schedulerSetting.sysMetricsGet.disable"
                              v-tooltip:top="'默认开启，关闭后，在工具-系统监控页面中将不再显示任何内容'"
-                             inline>
+                             inline color="error">
                 <template #prepend>
                   <span>
                     系统监控开关
@@ -96,7 +96,7 @@
             <v-col>
               <v-radio-group v-model="systemSettingForm.schedulerSetting.autoUpdate.enable"
                              v-tooltip:top="'默认开启，会在指定的时间检查游戏是否有更新，如果有则会自动更新'"
-                             inline class="mt-2">
+                             inline color="error" class="mt-2">
                 <template #prepend>
                   <span>
                     自动更新开关
@@ -132,7 +132,7 @@
             <v-col>
               <v-radio-group v-model="systemSettingForm.schedulerSetting.playerUpdateMod.disable"
                              v-tooltip:top="'默认开启，开启后游戏中的玩家可在聊天框中控制服务器进行模组更新'"
-                             inline class="mt-2">
+                             inline color="error" class="mt-2">
                 <template #prepend>
                   <span>
                     玩家更新模组开关
@@ -150,7 +150,7 @@
                               v-tooltip:top="'单位分钟，默认为10分钟，即发现有模组需要更新后，每10分钟提示一次（太频繁可能会很讨厌'"
                               :min="1"
                               :disabled="systemSettingForm.schedulerSetting.UIDMaintain.disable"
-                              control-variant="stacked" inset label="玩家更新模组检查频率" variant="outlined">
+                              control-variant="hidden" inset label="玩家更新模组检查频率" variant="outlined">
                 <template #append-inner>
                   <div style="width: 50px">分钟</div>
                 </template>
@@ -170,7 +170,7 @@
             <v-col>
               <v-radio-group v-model="systemSettingForm.sysSetting.autoRestart.enable"
                              v-tooltip:top="'默认开启，会在指定的时间重启服务器，自动更新模组'"
-                             inline class="mt-2">
+                             inline color="success" class="mt-2">
                 <template #prepend>
                   <span>
                     自动重启开关
@@ -206,7 +206,7 @@
             <v-col>
               <v-radio-group v-model="systemSettingForm.sysSetting.autoBackup.enable"
                              v-tooltip:top="'默认开启，会在指定的时间备份服务器，备份路径为~/dmp_files/backup/[集群名]'"
-                             inline class="mt-2">
+                             inline color="success" class="mt-2">
                 <template #prepend>
                   <span>
                     自动备份开关
@@ -242,7 +242,7 @@
             <v-col>
               <v-radio-group v-model="systemSettingForm.sysSetting.backupClean.enable"
                              v-tooltip:top="'默认关闭，会定时清理自动、手动创建的存档备份文件'"
-                             inline class="mt-2">
+                             inline color="success" class="mt-2">
                 <template #prepend>
                   <span>
                     备份清理开关
@@ -260,7 +260,7 @@
                               v-tooltip:top="'需要设置大于0的数字，如设置30，则会删除30天前创建的备份文件'"
                               :min="1"
                               :disabled="!systemSettingForm.sysSetting.backupClean.enable"
-                              control-variant="stacked" inset label="备份保留天数" variant="outlined">
+                              control-variant="hidden" inset label="备份保留天数" variant="outlined">
                 <template #append-inner>
                   <div style="width: 50px">天</div>
                 </template>
@@ -276,7 +276,7 @@
             <v-col>
               <v-radio-group v-model="systemSettingForm.sysSetting.keepalive.enable"
                              v-tooltip:top="'关闭后不会处理失效的世界'"
-                             inline class="mt-2">
+                             inline color="success" class="mt-2">
                 <template #prepend>
                   <span>
                     自动保活开关
@@ -294,7 +294,7 @@
                               v-tooltip:top="'单位分钟，默认为30分钟，即每30分钟检测一次世界是否正常，如果异常则会重启对应的世界，对其他正常运行的世界无影响'"
                               :min="1"
                               :disabled="!systemSettingForm.sysSetting.keepalive.enable"
-                              control-variant="stacked" inset label="自动保活检测频率" variant="outlined">
+                              control-variant="hidden" inset label="自动保活检测频率" variant="outlined">
                 <template #append-inner>
                   <div style="width: 50px">分钟</div>
                 </template>
@@ -310,7 +310,7 @@
             <v-col>
               <v-radio-group v-model="systemSettingForm.sysSetting.scheduledStartStop.enable"
                              v-tooltip:top="'默认关闭；如使用此功能，请关闭定时重启、自动保活'"
-                             inline class="mt-2">
+                             inline color="success" class="mt-2">
                 <template #prepend>
                   <span>
                     定时开启关闭集群开关
@@ -363,7 +363,7 @@
               <v-radio-group v-model="systemSettingForm.sysSetting.bit64"
                              :disabled="OSPlatform==='darwin'"
                              v-tooltip:top="'默认关闭，开启后会提升游戏性能，但会增加内存占用，请酌情开启'"
-                             inline class="mt-2">
+                             inline color="success" class="mt-2">
                 <template #prepend>
                   <span>
                     64位启动开关
@@ -383,7 +383,7 @@
             <v-col>
               <v-radio-group v-model="systemSettingForm.sysSetting.tickRate"
                              v-tooltip:top="'越大游戏越流畅，但会带来更高的带宽和CPU消耗（需要客户端同时配置'"
-                             inline class="mt-2">
+                             inline color="success" class="mt-2">
                 <template #prepend>
                   <span>
                     Tick Rate
